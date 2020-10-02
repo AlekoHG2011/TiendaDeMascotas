fe478ab9f3fff27eb1bd59b10a2c999e1c1d6d17
Alejandro Hechavarria Gonzalez <alejandrohg2011@gmail.com>
2020-10-01 22:25:50 -0400

    --- With functional raiting icon and see raiting text view.

--- app/src/main/java/com/example/aleko/tiendamascotas/MascotasFavoritas.java
+++ app/src/main/java/com/example/aleko/tiendamascotas/MascotasFavoritas.java
@@ -6,13 +6,23 @@ package com.example.aleko.tiendamascotas;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MascotasFavoritas extends AppCompatActivity {
+
+    ArrayList<Mascota> mascotas;
+    private RecyclerView listaMascotas;
+    public MascotaAdaptador adaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +31,30 @@ public class MascotasFavoritas extends AppCompatActivity {
 
         Toolbar miActionBar = (Toolbar) findViewById(R.id.miActionBar);
         setSupportActionBar(miActionBar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);   /// Activando navegacion hacia atras
+
         this.setTitle("Mascotas Favoritas");
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);   /// Activando navegacion hacia atras
+        FloatingActionButton moveTop = (FloatingActionButton) findViewById(R.id.top);
+        listaMascotas = (RecyclerView) findViewById(R.id.rvMascotas);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+
+        //GridLayoutManager glm = new GridLayoutManager(this, 2);
+
+        moveTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listaMascotas.smoothScrollToPosition(0);
+            }
+        });
+
+        listaMascotas.setLayoutManager(llm);
+        inicializarlistaMascotas();
+        inicializarAdaptador();
+
+
     }
 
     @Override
@@ -57,6 +88,24 @@ public class MascotasFavoritas extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void inicializarAdaptador() {
+        //MascotaAdaptador adaptador = new MascotaAdaptador(mascotas);
+        adaptador = new MascotaAdaptador(mascotas, this);
+        listaMascotas.setAdapter(adaptador);
+
+    }
+
+    public void inicializarlistaMascotas() {
+
+        mascotas = new ArrayList<Mascota>();
+
+        mascotas.add(new Mascota("Titi", "12 meses", R.drawable.d4, 0));
+        mascotas.add(new Mascota("Peggy", "18 meses", R.drawable.c1, 0));
+        mascotas.add(new Mascota("Yoyo", "1 mes", R.drawable.rabbit, 0));
+        mascotas.add(new Mascota("Maximus", "2 años", R.drawable.d7, 0));
+        mascotas.add(new Mascota("Odoo", "12 meses", R.drawable.cooker, 0));
     }
 
 }
